f3e2b91e6f894fe4f478bb1b88a5f7b4dfc84afa
Diffblue JCover <jcover@diffblue.com>
2019-12-14 14:31:44 +0000

    Unit tests generated by JCover

---
+++ src/test/java/com/diffblue/javademo/SearchTest.java
@@ -0,0 +1,40 @@
+package com.diffblue.javademo;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for com.diffblue.javademo.Search
+ */
+
+public class SearchTest {
+
+    @Test
+    public void containsArrayIsEmpty() {
+        assertFalse((new Search()).contains(new int[] { }, 1));
+    }
+
+    @Test
+    public void containsArrayIsOneReturnsTrue() {
+        int[] array = new int[] { 1 };
+        assertTrue((new Search()).contains(array, 1));
+    }
+
+    @Test
+    public void containsArrayIsZero() {
+        int[] array = new int[] { 0 };
+        assertFalse((new Search()).contains(array, 1));
+    }
+
+    @Test
+    public void isNeedleInHaystackInputIsFooReturnsFalse() {
+        assertFalse((new Search()).isNeedleInHaystack("foo"));
+    }
+
+    @Test
+    public void someNewMethodReturnsTrue() {
+        assertTrue((new Search()).someNewMethod());
+    }
+}
